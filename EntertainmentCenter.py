8a6965d23373e6a2542a1a41ad1d5dd819405eaa
BePitch <bpitchfo@gmail.com>
2017-06-04 16:29:58 -0400

    Files

---
+++ EntertainmentCenter.py
@@ -0,0 +1,34 @@
+import fresh_tomatoes
+import media
+
+# Movie #1
+toy_story = media.Movie("Toy Story 3",
+                        "A story of a boy and his toys",
+                        "https://upload.wikimedia.org/wikipedia/en/6/69/Toy_Story_3_poster.jpg",
+                        "https://www.youtube.com/watch?v=ZZv1vki4ou4"
+                        )
+# Movie #2
+avatar = media.Movie("Avatar",
+                     "a marine on a planet fighting for resources",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY"
+                     )
+# Movie #3    
+harry_potter = media.Movie("Harry Potter and the Deathly Hallows Part 2",
+                     "Fantasy movie that takes place in the real world about a group of kids",
+                     "https://upload.wikimedia.org/wikipedia/en/d/df/"
+                           "Harry_Potter_and_the_Deathly_Hallows_%E2%80%93_Part_2.jpg",
+                     "https://www.youtube.com/watch?v=9hXH0Ackz6w"
+                     )
+# Movie #4
+step_brothers = media.Movie("Step Brothers",
+                     "a comedy about the most amazing friendship that would"
+                            "spawn out of the most uncertain circumstances.",
+                     "https://upload.wikimedia.org/wikipedia/en/d/d9/StepbrothersMP08.jpg",
+                     "https://youtu.be/CewglxElBK0"
+                     )
+# Data Structure
+movies = [toy_story,avatar,harry_potter,step_brothers]
+
+# Build the Website and pass through each movie
+fresh_tomatoes.open_movies_page(movies)
---
+++ README_movies.txt
@@ -0,0 +1,11 @@
+There are three files. Run the third file to display the Movie Trailer Website.
+
+1) Media.py
+	-This builds the class Movie()
+
+2)fresh_tomatoes.py
+	-Builds html file
+	-Altered some CSS
+
+3)EntertainmentCenter.py (Execute or Run this file)
+	-Builds each movie that will be passed through into the website
